7bc0d03e7a1dd8b39ad17deddb15f1f0fc52107f
muh21akg <muhakg@yahoo.com>
2023-08-11 05:44:15 -0700

    x

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print lower and upper-case alphabets
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	char x = 'a';
+	char y = 'A';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
