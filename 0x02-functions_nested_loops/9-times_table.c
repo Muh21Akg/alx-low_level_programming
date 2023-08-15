178a4ff58ecece09e3f47bf377627cd44c50f5b3
muh21akg <muhakg@yahoo.com>
2023-08-15 09:31:29 -0700

    9 table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+/*
+ * File: 9-times_table.c
+ * Auth: Muhammad A Abdurrahman
+ */
+
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table
+ */
+void times_table(void)
+{
+	int i, j, x = i * j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar('x');
+			if (x != '9')
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('$');
+		_putchar('\n');
+	}
+}
