80761e7bc3d46a554bbdb510be1506b9335a7c59
muh21akg <muhakg@yahoo.com>
2023-08-15 10:21:20 -0700

    x

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
  * Auth: Muhammad A Abdurrahman
  */
 
-#include "main.h"
+#include <stdio.h>
 
 /**
  * print_to_98 - prints numbers form entered number up until 98
@@ -14,19 +14,12 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-		{
-			_putchar(n);
-			n--;
-		}
-		_putchar(n);
+			printf("%d, ", n--); 
+		printf("%d\n", n);
 	}
 	else
 	{
 		while (n < 98)
-		{
-			_putchar(n);
-			n++;
-		}
-		_putchar(n);
-	}
+			printf("%d, ", n++); 
+		printf("%d\n", n);
 }
