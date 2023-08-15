109f6ae996e352f16d74d1e8ade8a8c651035519
muh21akg <muhakg@yahoo.com>
2023-08-15 08:52:09 -0700

    24 hrs

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+/*
+ * File: 8-24_hours.c
+ * Auth: Muhammad A Abdurrahman
+ */
+
+#include "main.h"
+
+/**
+ * jack_bauer - prints minutes of the day of jack bauer from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int hr, mn;
+	for (hr = 0; hr <= 23; hr++)
+	{
+		for (mn = 0; mn <= 59; mn++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((mn / 10) + '0');
+			_putchar((mn % 10) + '0');
+		}
+	}
+}
