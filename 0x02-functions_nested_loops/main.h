404eb0a3284b55363835e038846ef506cead4cb0
muh21akg <muhakg@yahoo.com>
2023-08-15 04:25:33 -0700

    x

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h 
+ * Auth: Muhammad Abdurrahman
+ * Description: a header file that contains functions declaration for this project
+ */
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+
+#endif
