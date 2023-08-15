0cc7de0859bf90e405e8e7e7da598ae286ef5d8b
muh21akg <muhakg@yahoo.com>
2023-08-15 06:55:16 -0700

    betty doc

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
-/**
+/*
  * File: 1-alphabet.c
  * Auth: Muhammad A. Abdurrahaman
  */
+
 #include "main.h"
+
 /**
- * print_alphabet: prints the alphabet followed by a new line
+ * print_alphabet - prints the alphabet followed by a new line
  *
- * Return: 0 success
  */
 void print_alphabet(void)
 {
