b1b339da006d9947a08f21c2458273db94bdbe76
muh21akg <muhakg@yahoo.com>
2023-08-15 07:49:39 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+/*
+ * File: 6-abs.c
+ * Auth: Muhammad A Abdurrahman
+ */
+
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a given number
+ * @x: is the number to be processed
+ * Return: x success
+ */
+int _abs(int x)
+{
+	if (x >= 0)
+		return (x);
+	else
+		return ((-1)*x);
+}
