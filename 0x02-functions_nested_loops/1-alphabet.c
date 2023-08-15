25995c6b688162e686b6334ecbbda6ebd7e42259
muh21akg <muhakg@yahoo.com>
2023-08-15 04:54:07 -0700

    x

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,17 @@
+/**
+ * File: 1-alphabet.c
+ * Auth: Muhammad A. Abdurrahaman
 #include "main.h"
 /**
- * 1-alphabet prints the alphabet followed by a new line
+ * print_alphabet: prints the alphabet followed by a new line
  *
  * Return: 0 success
  */
 void print_alphabet(void)
 {
 	char c = 'a';
-	while ( c <= 'z')
+
+	while (c <= 'z')
 	{
 		_putchar(c);
 		c++;
