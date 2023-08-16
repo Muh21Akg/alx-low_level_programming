39f043a750788b34b0a982329bacfde6c033c656
muh21akg <muhakg@yahoo.com>
2023-08-16 06:55:49 -0700

    x

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,12 +2,12 @@
  * File: 101-natural.c
  * Auth: Muhammad A Abdurrahman
  */
-#include <stdio.h> 
+#include <stdio.h>
  /**
-  * main - prints the sum of all the multiples of 3 or 5 thar are less than 1024
+  * main - entry point
+  * prints the sum of all the multiples of 3 or 5 less than 1024
   * Return: 0 success
   */
-
 int main(void)
 {
 	int i, sum;
