8c83a27b8093fddf066fa9d08eaf8ea136d967d4
muh21akg <muhakg@yahoo.com>
2023-08-16 06:37:47 -0700

    sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+/*
+ * File: 101-natural.c
+ * Auth: Muhammad A Abdurrahman
+ */
+#include <stdio.h> 
+ /**
+  * main - prints the sum of all the multiples of 3 or 5 thar are less than 1024
+  * Return: 0 success
+  */
+
+int main(void)
+{
+	int i, sum;
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%d\n", sun);
+	return (0);
+}i
