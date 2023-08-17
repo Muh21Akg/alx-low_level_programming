ab0333fc049412e03a66d72f124e0a1eb4d4961d
muh21akg <muhakg@yahoo.com>
2023-08-17 03:02:46 -0700

    x

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -22,16 +22,14 @@ int main(void)
 			x /= 2;
 			continue;
 		}
-	}
 
-	for (i = 3; i < x / 2; i += 2)
-	{
-		if (x % i == 0)
+		for (i = 3; i < x / 2; i += 2)
 		{
-			x /= i;
+			if (x % i == 0)
+				x /= i;
+
 		}
 	}
 	printf("%ld\n", x);
-
 	return (0);
 }
