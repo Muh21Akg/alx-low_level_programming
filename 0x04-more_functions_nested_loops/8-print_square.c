3dfaff6537163f928e00aec08d37f6881ab9623d
muh21akg <muhakg@yahoo.com>
2023-08-17 07:01:29 -0700

    x

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,13 +13,19 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = size; i >= 1; i--)
+	if (size > 0)
 	{
-		for (j = size; j >= 1; j--)
+		for (i = 0; i < size; i++)
 		{
+			for (j = 0; j < (size - 1); j++)
+				_putchar('#');
+
 			_putchar('#');
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
