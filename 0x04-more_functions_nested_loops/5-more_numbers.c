41adb0ec722dde761c332e526aebc0d23a529e37
muh21akg <muhakg@yahoo.com>
2023-08-17 06:36:22 -0700

    x

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,13 @@ void more_numbers(void)
 
 	for (n1 = 0; n1 < 10; n1++)
 	{
-		for (number2 = 0; number2 <= 14; number2++)
+		for (n2 = 0; n2 <= 14; n2++)
 		{
-			if (number2 > 9)
+			if (n2 > 9)
 			{
-				_putchar((number2 / 10) + '0');
+				_putchar((n2 / 10) + '0');
 			}
-			_putchar((number2 % 10) + '0');
+			_putchar((n2 % 10) + '0');
 		}
 		_putchar(10);
 	}
