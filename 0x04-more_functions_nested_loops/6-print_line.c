af7966dfcd97eaf4e5d4ec887c8512c4eef60789
muh21akg <muhakg@yahoo.com>
2023-08-17 06:44:03 -0700

    x

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -13,12 +13,9 @@ void print_line(int n)
 {
 	int i;
 
-	if (n > 0)
+	for (i = n; i >= 1; i--)
 	{
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
 	}
 	_putchar('\n');
 }
