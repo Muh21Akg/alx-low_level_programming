91ad3519eb2bdb07b84d881f799fd7fe6223f659
muh21akg <muhakg@yahoo.com>
2023-08-21 05:12:04 -0700

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the 2nd half of a string
+ * @str: holds the address
+ */
+void puts_half(char *str)
+{
+	int x, y, z;
+
+	for (x = 0; str[x] != '\0'; x++)
+		;
+	if (x % 2 == 0)
+	{
+		y = x / 2;
+	}
+	else
+	{
+		y = (x - 1) / 2;
+	}
+	for (z = y; str[z] != '\0'; z++)
+	{
+		_putchar(str[z]);
+	}
+	_putchar('\n');
+}
