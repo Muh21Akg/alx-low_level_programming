9400c86530f758bc416fb4ddbf94e596c2b3fb56
muh21akg <muhakg@yahoo.com>
2023-08-21 03:53:06 -0700

    str rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reversed order
+ * @s: holds the address
+ */
+void print_rev(char *s)
+{
+	int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+		;
+	for (y = x; y >= 0; y--)
+	{
+		_putchar(s[y]);
+	}
+	_putchar('\n');
+}
