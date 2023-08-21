3726acd9a11a817fb0743dba75cf6f11074134cd
muh21akg <muhakg@yahoo.com>
2023-08-21 04:29:40 -0700

    every other element

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints even chars
+ * @str: holds the address
+ */
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x % 2 == 0)
+		{
+			_putchar(str[x]);
+		}
+	}
+	_putchar('\n');
+}
