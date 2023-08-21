efecc9656820366c5bd78638962931f4b37e34ee
muh21akg <muhakg@yahoo.com>
2023-08-21 05:47:55 -0700

    betty styling

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,12 +9,12 @@ void print_array(int *a, int n)
 {
 	int x;
 
-	for ( x = 0; x < n; x++)
+	for (x = 0; x < n; x++)
 	{
 		printf("%d", a[x]);
-		if (x != (n - 1)) 
-		{ 
-			printf(", "); 
+		if (x != (n - 1))
+		{
+			printf(", ");
 		}
 	}
 	printf("\n");
