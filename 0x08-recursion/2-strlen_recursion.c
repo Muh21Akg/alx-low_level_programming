4d9993d4d47efe11f2631ce7875200d78b5def8c
muh21akg <muhakg@yahoo.com>
2023-08-30 11:32:45 -0700

    strlength

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - calculates the length of a string
+ * @s: holds the address
+ * Return: legth of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\n')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
