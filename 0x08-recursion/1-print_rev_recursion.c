39468f624297d9f93c42b8615ad66eb599c8d4b7
muh21akg <muhakg@yahoo.com>
2023-08-30 11:14:40 -0700

    string and reversed string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints reversed string with recursion
+ * @s: holds the address of string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
