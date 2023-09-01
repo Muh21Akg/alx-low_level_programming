452629b0a0f6318a1f4e49337900f6bb71e0729c
muh21akg <muhakg@yahoo.com>
2023-09-01 14:08:23 -0700

    coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int x, y, z, t, sum = 0;
+	int x, sum = 0;
 
 	if (argc != 2)
 	{
@@ -17,22 +17,30 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	x = atoi(argv[1]);
-	if (x / 25 > 0)
+	while (x >= 25)
 	{
-		sum += x / 25;
+		x -= 25;
+		sum += 1;
 	}
-	if (x / 10 > 0)
+	while (x >= 10)
 	{
-		y = x % 25;
-		sum += y / 10;
+		x -= 10;
+		sum += 1;
 	}
-	if (x / 5 > 0)
+	while (x >= 5)
 	{
-		z = y % 10;
-		sum += z / 5;
+		x -= 5;
+		sum += 1;
 	}
-	t = z % 5;
-	sum += t;
+	while (x >= 2)
+	{
+		x -= 2;
+		sum += 1;
+	}
+	if (x == 1)
+	{
+		sum += 1;
+	}
 	printf("%d\n", sum);
 	return (0);
 }
