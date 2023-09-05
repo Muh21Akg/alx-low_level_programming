1c9122030a45276fdb781d3137df5c801dae72c5
muh21akg <muhakg@yahoo.com>
2023-09-05 01:31:52 -0700

    x

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,12 +12,14 @@ char *str_concat(char *s1, char *s2)
 	int i, size1, size2;
 	char *con;
 
+	if (s1 == NULL)
+		s1 = " ";
+	if (s2 == NULL)
+		s2 = " ";
 	for (size1 = 0;  s1[size1] != '\0'; size1++)
 		;
 	for (size2 = 0; s2[size2] != '\0'; size2++)
 		;
-	if (size1 <= 0 || size2 <= 0)
-		return (NULL);
 	con = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (con == NULL)
 		return (NULL);
