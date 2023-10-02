d63d9616dc5e8f02f74d9609642e16344c0f2c61
muh21akg <muhakg@yahoo.co>
2023-10-02 05:00:22 -0700

    dumb mistake

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,10 +4,12 @@
 /**
  * mqin - prints no of arguments
  * @argc: first argument
+ * @argv: second argument
  * Return: 0 success
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
+	argv++;
 	printf("%d\n", argc);
 	return (0);
 }
