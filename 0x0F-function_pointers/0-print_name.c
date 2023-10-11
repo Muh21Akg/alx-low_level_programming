834eab46a4d796afabd1ca8887f792d59106c6f0
muh21akg <muhakg@yahoo.com>
2023-10-11 05:16:55 -0700

    first task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a given name
+ * @name: points to the name to be printed
+ * @f: points to a function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
